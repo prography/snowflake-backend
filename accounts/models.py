bf70c003a91eba17f95a25fd077e29ef66b87f69
jisoo <cjisoo0117@gmail.com>
2020-05-07 00:30:55 +0900

    update image file name

--- accounts/models.py
+++ accounts/models.py
@@ -11,13 +11,13 @@ from .managers import UserManager
 
 def create_image_path(instance, filename):
     # upload_to="%Y/%m/%d" 처럼 날짜로 세분화
-    ymd_path = timezone.now().strftime("%Y/%m/%d")
+    ymd_path = timezone.now().strftime("%Y-%m-%d")
     # 길이 32 인 uuid 값
     uuid_name = uuid4().hex
     # 확장자 추출
     extension = os.path.splitext(filename)[-1].lower()
     # 결합 후 return
-    return "/".join(["user", ymd_path, uuid_name + extension])
+    return "/".join(["user", ymd_path + uuid_name + extension])
 
 
 class User(AbstractUser):
