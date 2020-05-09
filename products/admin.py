a8096a21cc61ef7aeb2e23a9e1eaff7b64a7a5eb
JeongTaek Lim <ljt1100@gmail.com>
2020-05-09 14:37:04 +0900

    content_type 적용 및 admin 일부 수정

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, Condom, Gel
 
 # Register your models here.
+admin.site.register(Product)
+
+admin.site.register(Condom)
+
+admin.site.register(Gel)
