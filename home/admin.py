63c7265b2026cc6d9bab15a6746f78a1945ddb5a
jisoo <cjisoo0117@gmail.com>
2020-05-10 16:20:29 +0900

    WelcomeCard admin 이미지 미리보기 등록 완료

--- home/admin.py
+++ home/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
+
 from .models import WelcomeCard
 
 
 @admin.register(WelcomeCard)
 class WelcomeCardAdmin(admin.ModelAdmin):
-    # # Image tag for admin page
-    # def image_tag(self, obj):
-    #     return mark_safe('<img src="%s" width="150" height="150" />' % (obj.image.url))
-    #
-    # image_tag.short_description = 'Image'
-    # fields = ['image', 'image_tag'] + [field.name for field in User._meta.fields if
-    #                                    field.name != "id" and field.name != 'image']
-    # readonly_fields = ('image_tag',)
-    fields = [field.name for field in WelcomeCard._meta.fields if field.name != "id"]
-    readonly_fields = ('created_at', 'updated_at')
+    # Image tag for admin page
+    def image_tag(self, obj):
+        return mark_safe('<img src="%s" width="150" height="150" />' % (obj.image.url))
+
+    image_tag.short_description = "Image 미리보기"
+    fields = ["title", "image", "image_tag"] + [
+        field.name
+        for field in WelcomeCard._meta.fields
+        if field.name != "id" and field.name != "image" and field.name != "title"
+    ]
+    readonly_fields = ("image_tag", "created_at", "updated_at")
+    # fields = [field.name for field in WelcomeCard._meta.fields if field.name != "id"]
