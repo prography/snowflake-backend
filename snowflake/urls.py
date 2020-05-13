3eaa4b972a5bfcb150468f770f4fd19e29439d7f
jisoo <cjisoo0117@gmail.com>
2020-05-13 19:51:51 +0900

    url 수정

--- snowflake/urls.py
+++ snowflake/urls.py
@@ -39,15 +39,19 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    # swagger
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+    # admin
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
-    path('home/', include('home.urls', namespace='home')),
 
     # JWT v2: simple jwt token
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # app
+    path('accounts/', include('accounts.urls')),
+    path('home/', include('home.urls')),
 ]
