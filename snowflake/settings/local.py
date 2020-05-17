7827ec58840c7463665754d7415f47fc5ad91659
JeongTaek Lim <ljt1100@gmail.com>
2020-05-17 18:42:32 +0900

    local환경으로 변수 변경

--- snowflake/settings/local.py
+++ snowflake/settings/local.py
@@ -6,9 +6,16 @@ ALLOWED_HOSTS = [
 ]
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('MYSQL_NAME'),
+        'USER': env('MYSQL_USER'),
+        'PASSWORD': env('MYSQL_PASSWORD'),
+        'HOST': env('MYSQL_HOST'),
+        'PORT': env('MYSQL_PORT'),
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
