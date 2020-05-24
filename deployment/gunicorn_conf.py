1313ad91277dfc9dcf7cfd18a72975ce36a46ab5
jisoo <cjisoo0117@gmail.com>
2020-05-25 00:25:33 +0900

    Docker 관련 설정 #7
    gunicorn, supervisord 수정

--- deployment/gunicorn_conf.py
+++ deployment/gunicorn_conf.py
@@ -1,6 +1,5 @@
 import multiprocessing
 
-name = 'snowflake'
 bind = 'unix:/app/snowflake.sock'
 workers = multiprocessing.cpu_count() * 2 + 1
 keepalive = 32
@@ -15,9 +14,5 @@ max_requests = 1000
 max_requests_jitter = 50
 graceful_timeout = 15
 timeout = 15
-
-BASE_DIR = "/app/snowflake"
-pythonpath = BASE_DIR
-chdir = BASE_DIR
 
 preload_app = False
