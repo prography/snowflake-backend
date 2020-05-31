c99c8e254addcdc38fa7b107227229fb09916c4a
JeongTaek Lim <ljt1100@gmail.com>
2020-05-31 16:35:49 +0900

    좋아요 생성 API 작성

--- likes/views.py
+++ likes/views.py
@@ -1,26 +1,38 @@
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from likes.models import Like
 from likes.serializers.like import LikeSerializer
 
 
 class LikeViewSet(viewsets.ModelViewSet):
-    # def get_permissions(self):
-    #     if self.action == 'create':
     permission_classes = [IsAuthenticated]
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
 
-    # def create(self, request, *args, **kwargs):
-    #     serializer = self.get_serializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #
-    #     self.perform_create(serializer)
-    #     return Response({"message": "회원가입 완료!"}, status=status.HTTP_201_CREATED)
-    # def delete(self, request, *args, **kwargs):
-    #     print("1")
-    #     pass
+    def create(self, request, *args, **kwargs):
+        """
+        새 좋아요를 만듭니다.
+        """
+        data = {'model': request.data.get('model'),
+                'object_id': int(request.data.get('object_id')),
+                'user': int(request.data.get('user'))}
+        model = data.pop('model')
+        try:
+            ct = ContentType.objects.get(model=model)
+        # django.contrib.contenttypes.models.ContentType.DoesNotExist:
+        except:
+            return Response("content_type 이름이 잘못되었습니다.", status=status.HTTP_400_BAD_REQUEST)
+        data['content_type'] = ct.id
+
+        serializer = LikeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
