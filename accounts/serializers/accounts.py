34d32b0feb13e5154af33c19b096715d9eacd6a8
jisoo <cjisoo0117@gmail.com>
2020-06-01 17:25:10 +0900

    password 1개만 받게 수정

--- accounts/serializers/accounts.py
+++ accounts/serializers/accounts.py
@@ -5,16 +5,15 @@ from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
 class UserSerializer(serializers.ModelSerializer):
-    password1 = serializers.CharField(max_length=100, write_only=True, style={"input_type": "password"})
-    password2 = serializers.CharField(max_length=100, write_only=True, style={"input_type": "password"})
+    password = serializers.CharField(max_length=100, write_only=True, style={"input_type": "password"})
+    # password2 = serializers.CharField(max_length=100, write_only=True, style={"input_type": "password"})
 
     class Meta:
         model = User
         fields = [
             "id",
             "email",
-            "password1",
-            "password2",
+            "password",
             "username",
             "birth_year",
             "image",
@@ -26,14 +25,10 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"date_joined": {"read_only": True}}
 
     def create(self, validated_data):
-        password1 = validated_data.pop("password1", "")
-        password2 = validated_data.pop("password2", "")
-
-        if password1 and password2 and password1 != password2:
-            raise serializers.ValidationError("비밀번호와 비밀번호 확인이 일치하지 않습니다.")
+        password = validated_data.pop("password", "")
 
         user = User.objects.create(**validated_data)
-        user.set_password(password1)
+        user.set_password(password)
         user.save()
         return user
 
