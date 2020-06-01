ce132c38faf98772e8a181f22007da30a5caf7fe
jisoo <cjisoo0117@gmail.com>
2020-06-01 17:20:16 +0900

    condom url 수정

--- products/urls.py
+++ products/urls.py
@@ -14,7 +14,7 @@ app_name = "products"
 
 urlpatterns = [
     path("welcome-cards/", WelcomeCardListReadView.as_view(), name="welcome-cards"),
-    path("condom/list/", CondomListView.as_view(), name="condom-list"),
+    path("condom/", CondomListView.as_view(), name="condom-list"),
     path("condom/top-n/", CondomTopNListView.as_view(), name="condom-top-n"),
     path("condom/trio/", CondomTrioView.as_view(), name="condom-trio"),
     path("condom/<int:pk>/", CondomDetailView.as_view(), name="condom-detail"),
