6abe9f194650cef2f59f6976fe66698399a77eee
Jichang Jang <devjang@crossangle.io>
2020-06-13 21:54:04 +0900

    소셜 로그인 리팩토링 - 카카오, 네이버 소셜 로그인을 클래스로 변경

--- accounts/views.py
+++ accounts/views.py
@@ -1,21 +1,20 @@
 import json
-import uuid
+from uuid import uuid4
 
 import requests
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from rest_framework import status, viewsets
+from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
-from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from accounts.models import User
 from accounts.serializers.accounts import (CustomUserObtainPairSerializer,
                                            UserSerializer)
-from accounts.social_login import kakao, naver
-
+from accounts.social_login.kakao_social_login import KakaoSocialLogin
+from accounts.social_login.naver_social_login import NaverSocialLogin
 
 class AnonCreateAndUpdateOwnerOnly(permissions.BasePermission):
     """
@@ -34,13 +33,15 @@ class AnonCreateAndUpdateOwnerOnly(permissions.BasePermission):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    # def get_permissions(self):
-    #     if self.action == 'create':
-    # permission_classes = [AnonCreateAndUpdateOwnerOnly]
     permission_classes = [permissions.AllowAny]
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    state_token_code = uuid.uuid4()
+    state_token_code = uuid4().hex
+
+    def __init__(self, **kwargs):
+        self.kakao_social_login = KakaoSocialLogin()
+        self.naver_social_login = NaverSocialLogin(self.state_token_code)
+        super(UserViewSet, self).__init__(**kwargs)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -50,30 +51,27 @@ class UserViewSet(viewsets.ModelViewSet):
         return Response({"message": "회원가입 완료!"}, status=status.HTTP_201_CREATED)
 
     @action(detail=False, methods=['get'], url_path='kakao-login')
-    def kakao_login(self, request, pk=None):
-        app_key = settings.KAKAO_APP_KEY
-        redirect_uri = settings.KAKAO_REDIRECT_URI
-        url = f'https://kauth.kakao.com/oauth/authorize?client_id={app_key}&redirect_uri={redirect_uri}&response_type=code&scope=account_email,gender,birthday'
+    def get_kakao_auth_token(self, request, pk=None):
+        url = self.kakao_social_login.get_auth_url()
         return redirect(url)
 
     @action(detail=False, methods=['get'], url_path='kakao-login-callback')
-    def kakao_login_callback(self, request, **kwargs):
+    def kakao_login_callback(self, request, pk=None):
         try:
-            code = kakao.get_code(request)
-            access_token = kakao.get_access_token(code)
-            user_response = kakao.get_user_response(access_token)
-            snowflake_user_data = kakao.get_data_for_snowflake(user_response.json())
+            user_data_per_field = self.kakao_social_login.get_user_data(request)
         except Exception as e:
-            if e.args:
-                detail = e.args[0]
-                error_name = e.__class__.__name__
-            return Response({"message": f'{error_name}가 발생했습니다. {detail}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return self.error_with_message(e)
 
-        user = self._kakao_login_or_sign_up(snowflake_user_data)
-        if not isinstance(user, User):
+        if self._have_already_sign_up_for_other_social(user_data_per_field):
+            what_social_did_user_already_sign_up = User.objects.get(email=user_data_per_field['email']).social
             return Response({
-                "message": f"이미 {user}로 회원가입을 하셨습니다. 다시 확인해주세요."
-            })
+                'message': f'이미 {what_social_did_user_already_sign_up}로 가입했습니다. {what_social_did_user_already_sign_up}로 로그인 해주세요.'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(email=user_data_per_field['email'], social=user_data_per_field['social']):
+            user = self.kakao_social_login.login(user_data_per_field)
+        else:
+            user = self.kakao_social_login.sign_up(user_data_per_field)
 
         refresh = CustomUserObtainPairSerializer.get_token(user)
         return Response({
@@ -81,60 +79,48 @@ class UserViewSet(viewsets.ModelViewSet):
             'access': str(refresh.access_token)
         })
 
-    def _kakao_login_or_sign_up(self, snowflake_user_data):
-        email = snowflake_user_data['email']
-        social = snowflake_user_data['social']
-        user = User.objects.filter(email=email)
+    def error_with_message(self, e):
+        if e.args:
+            detail = e.args[0]
+            error_name = e.__class__.__name__
+        return Response({"message": f'{error_name}가 발생했습니다. {detail}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        if user.exists():
-            user = User.objects.get(email=email)
-            if user.social == social:
-                user = kakao.login(snowflake_user_data)
-                return user
-            else:
-                already_signup_social = user.social
-                if already_signup_social == 'NONE':
-                    already_signup_social = "눈송이"
-                return already_signup_social
-
-        user = kakao.sign_up(snowflake_user_data)
-        return user
-
+    def _have_already_sign_up_for_other_social(self, user_data_per_field):
+        user = User.objects.filter(email=user_data_per_field['email'])
+        if user.count() > 1:
+            raise AssertionError('해당 이메일로 가입된 계정이 이미 존재합니다. 관리자에게 문의해주세요.')
+        if user.count() == 1:
+            user = user[0]
+            return user.social != user_data_per_field['social']
+        return False
+    
     @action(detail=False, methods=['get'], url_path='naver-login')
-    def naver_login(self, request, pk=None):
-        state_token_code = self.state_token_code
-        client_id = settings.NAVER_APP_KEY
-        callback_url = settings.NAVER_REDIRECT_URI
-
-        url = f'https://nid.naver.com/oauth2.0/authorize?client_id={client_id}&response_type=code&redirect_uri={callback_url}&state={state_token_code}'
+    def get_naver_auth_token(self, request, pk=None):
+        url = self.naver_social_login.get_auth_url()
         return redirect(url)
         
     @action(detail=False, methods=['get'], url_path='naver-login-callback')
-    def naver_login_callback(self, request, **kwargs):
+    def naver_login_callback(self, request, pk=None):
         callback_status_token_code = request.query_params.get('state')
-        if callback_status_token_code != self.state_token_code:
-            Response({'message': 'state token code is not valid'}, status=status.HTTP_401_UNAUTHORIZED)
+        if callback_status_token_code != self.naver_social_login.state_token_code:
+            return Response({'message': 'state token code is not valid'}, status=status.HTTP_401_UNAUTHORIZED)
         
         try:
-            # client_id = settings.NAVER_APP_KEY
-            # client_secret = settings.NAVER_APP_SECRET_KEY
-            state = self.state_token_code
-            code = naver.get_code(request)
-            access_token, token_type = naver.get_access_token(code, state)
-            user_response = naver.get_user_response(access_token, token_type)
-            snowflake_user_data = naver.get_data_for_snowflake(user_response.json())
+            user_data_per_field = self.naver_social_login.get_user_data(request)
         except Exception as e:
-            if e.args:
-                detail = e.args[0]
-                error_name = e.__class__.__name__
-            return Response({"message": f'{error_name}가 발생했습니다. {detail}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        user = self._naver_login_or_sign_up(snowflake_user_data)
-        if not isinstance(user, User):
+            return self.error_with_message(e)
+
+        if self._have_already_sign_up_for_other_social(user_data_per_field):
+            what_social_did_user_already_sign_up = User.objects.get(email=user_data_per_field['email']).social
             return Response({
-                "message": f"이미 {user}로 회원가입을 하셨습니다. 다시 확인해주세요."
-            })
+                'message': f'이미 {what_social_did_user_already_sign_up}로 가입했습니다. {what_social_did_user_already_sign_up}로 로그인 해주세요.'
+            }, status=status.HTTP_400_BAD_REQUEST)
         
+        if User.objects.filter(email=user_data_per_field['email'], social=user_data_per_field['social']):
+            user = self.naver_social_login.login(user_data_per_field)
+        else:
+            user = self.naver_social_login.sign_up(user_data_per_field)
+
         refresh = CustomUserObtainPairSerializer.get_token(user)
         return Response({
             'refresh': str(refresh),
