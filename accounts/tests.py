7bf91bcf5b8dbd70d2c2eb59354f792b3340ed53
jisoo <cjisoo0117@gmail.com>
2020-06-20 16:59:09 +0900

    user test 코드

--- accounts/tests.py
+++ accounts/tests.py
@@ -6,16 +6,34 @@ from rest_framework import status
 
 
 class UserTestCase(APITestCase):
+    def setUp(self):
+        user = User.objects.create(email="1@1.com", password="123")
+
     def test_create_user(self):
         url = reverse("accounts:user-list")
         data = {
-            "email": "1@1.com",
-            "password": "123",
+            "email": "2@2.com",
+            "password1": "123",
+            "password2": "123",
             "username": "남바완",
-            "image": ImageFile(open("media/1.png", "rb")),
-            "social": "",
-            "gender": 1,
-            "partner_gender": 1,
         }
         response = self.client.post(url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_check_unique_username(self):
+        url = reverse("accounts:user-list")
+        data = {
+            "email": "1@1.com",
+            "password1": "123",
+            "password2": "123",
+            "username": "남2바완",
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_user(self):
+        url = reverse("accounts:user-detail", kwargs={"pk": 1})
+        data = {"email": "1@2.com"}
+        response = self.client.patch(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(User.objects.get(pk=1).email, "1@2.com")
