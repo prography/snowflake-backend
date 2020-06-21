95b0ae09c2a1d644f22331c46ad98762c2c9cd63
JeongTaek Lim <ljt1100@gmail.com>
2020-06-21 16:20:40 +0900

    email, username 중복체크 엔드포인트 작성

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
-from django.urls import path, include
-from .views import UserViewSet
+from django.urls import path, include, re_path
+from .views import UserViewSet, check_duplicates_email, check_duplicates_username
 from rest_framework.routers import DefaultRouter
 
 app_name = 'accounts'
@@ -8,5 +8,7 @@ router = DefaultRouter()
 router.register(r'', UserViewSet)
 
 urlpatterns = [
+    re_path('check-duplicates/email/', check_duplicates_email),
+    re_path('check-duplicates/username/', check_duplicates_username),
     path('', include(router.urls))
 ]
