6e90a6a8bc75061ae46c38e94bdf3fbdda7a3a66
JeongTaek Lim <ljt1100@gmail.com>
2020-06-21 17:08:02 +0900

    User 업데이트, 삭제 API 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include, re_path
-from .views import UserViewSet, check_duplicates_email, check_duplicates_username
+from .views import UserViewSet, check_duplicates_email, check_duplicates_username, UserGetUpdateView
 from rest_framework.routers import DefaultRouter
 
 app_name = 'accounts'
@@ -10,5 +10,6 @@ router.register(r'', UserViewSet)
 urlpatterns = [
     re_path('check-duplicates/email/', check_duplicates_email),
     re_path('check-duplicates/username/', check_duplicates_username),
+    path('info/', UserGetUpdateView.as_view()),
     path('', include(router.urls))
 ]
