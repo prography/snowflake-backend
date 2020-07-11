a942a970b9501273c8978954cb9446fa1422ff80
JeongTaek Lim <ljt1100@gmail.com>
2020-07-11 16:21:19 +0900

    review 총점 업데이트 endpoint 수정

--- reviews/views.py
+++ reviews/views.py
@@ -10,7 +10,7 @@ from accounts.models import User
 from reviews.models import ReviewCondom, Review, ReviewGel
 from products.models import Condom
 
-from django.db.models import F
+from django.db.models import F, Avg
 
 from snowflake.exception import MissingProductIdException
 
@@ -26,16 +26,16 @@ class AnonCreateAndUpdateOwnerOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         # 익명 유저를 위한 조회
         return (
-            view.action in ["list", "retrieve"] or request.user and request.user.is_authenticated
+                view.action in ["list", "retrieve"] or request.user and request.user.is_authenticated
         )  # 유저에 의한 수정
 
     def has_object_permission(self, request, view, obj):
         if view.action in ["list", "retrieve"]:
             return True
         return (
-            view.action in ["create", "update", "partial_update"]
-            and obj.id == request.user.id
-            or request.user.is_staff
+                view.action in ["create", "update", "partial_update"]
+                and obj.id == request.user.id
+                or request.user.is_staff
         )
 
 
@@ -88,38 +88,19 @@ class UpdateCondomScore(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, format=None):
-        # 점수를 hash에 담아두기
-        condom = {}
-        for review in ReviewCondom.objects.all():
-            review_score = [review.total, review.oily, review.thickness, review.durability, 1]
-            product = review.product.id
-            if product in condom:
-                old_score = condom[product]
-                new_score = []
-                for c, t in zip(old_score[:-1], review_score[:-1]):
-                    new = c + t
-                    new_score.append(new)
-                new_score.append(old_score[-1] + 1)
-                condom[product] = new_score
-            else:
-                condom[product] = review_score
+        condoms = Condom.objects.all()
+        for condom in condoms:
+            # ReviewCondom filtering
+            queryset = ReviewCondom.objects.filter(product__id=condom.id)
+            # num_of_reviews update
+            queryset_cnt = queryset.count()
+            condom.num_of_reviews = queryset_cnt
+            # average score update
+            aggs_result = queryset.aggregate(Avg('oily'), Avg('thickness'), Avg('durability'), Avg('total'))
+            condom.avg_durability = aggs_result['durability__avg'] if aggs_result['durability__avg'] is not None else 0
+            condom.avg_oily = aggs_result['oily__avg'] if aggs_result['oily__avg'] is not None else 0
+            condom.avg_thickness = aggs_result['thickness__avg'] if aggs_result['thickness__avg'] is not None else 0
+            condom.score = aggs_result['total__avg'] if aggs_result['total__avg'] is not None else 0
+            condom.save()
 
-        # 점수 업데이트
-        for c in Condom.objects.all():
-            key = c.id
-
-            # 리뷰가 없는 콘돔 건너뛰기
-            if c.id not in condom:
-                continue
-
-            num_of_reviews = condom[key][4]
-
-            c.score = condom[key][0] / num_of_reviews
-            c.avg_oily = condom[key][1] / num_of_reviews
-            c.avg_thickness = condom[key][2] / num_of_reviews
-            c.avg_durability = condom[key][3] / num_of_reviews
-            c.num_of_reviews = num_of_reviews
-
-            c.save()
-
-        return Response(status=status.HTTP_200_OK, data=condom)
+        return Response("Complete", status=status.HTTP_200_OK)
