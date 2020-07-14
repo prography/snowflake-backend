c09ee945d56dc7d39286a5ac2fcfce52c1f21dfe
Jichang Jang <devjang@crossangle.io>
2020-07-14 13:46:41 +0900

    로컬 환경 센트리 설정 추가
    init 함수의 environment가 production이다.

--- snowflake/settings/production.py
+++ snowflake/settings/production.py
@@ -17,3 +17,12 @@ DATABASES = {
         }
     }
 }
+
+sentry_sdk.init(
+    dsn=env('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    environment='production'
+)
