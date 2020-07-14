5c1bea466acfacc2b170aaaf2ff4d9bf510af81f
Jichang Jang <devjang@crossangle.io>
2020-07-14 13:46:29 +0900

    로컬 환경 센트리 설정 추가
    init 함수의 environment가 local이다.

--- snowflake/settings/local.py
+++ snowflake/settings/local.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 DEBUG = True
@@ -12,3 +15,12 @@ DATABASES = {
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
+
+sentry_sdk.init(
+    dsn=env('SENTRY_DSN'),
+    integrations=[DjangoIntegration()],
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    environment='local'
+)
