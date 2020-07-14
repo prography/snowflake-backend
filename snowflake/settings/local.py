7fe979232281a60ea98eef11d2d36db5c6d2c3a1
Jichang Jang <devjang@crossangle.io>
2020-07-14 19:48:37 +0900

    센트리를 설정하라.
    KeyboardInterrupt 에러는 무시한다.

--- snowflake/settings/local.py
+++ snowflake/settings/local.py
@@ -16,11 +16,21 @@ DATABASES = {
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 
+def sentry_before_send(event, hint):
+    ignored_exceptions = [KeyboardInterrupt]
+    if 'exc_info' in hint:
+        _, exc_value, _ = hint['exc_info']
+
+        for exception in ignored_exceptions:
+            if isinstance(exc_value, exception):
+                return
+    return event
+
 sentry_sdk.init(
     dsn=env('SENTRY_DSN'),
     integrations=[DjangoIntegration()],
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
+    attach_stacktrace=True,
+    before_send=sentry_before_send,
     environment='local'
 )
