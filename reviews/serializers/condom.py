21f167741c75f9beb3d44bec55cd8155b947aaa0
Jisoo Choi <cjisoo0117@gmail.com>
2020-07-14 23:39:48 +0900

    review serializer likes 필드 명 likes_count로 변경

--- reviews/serializers/condom.py
+++ reviews/serializers/condom.py
@@ -20,22 +20,22 @@ condom_fields = [
     "thickness",
     "durability",
     "content",
-    "likes",
+    "likes_count",
 ]
 
 
 class ReviewCondomListSerializer(serializers.ModelSerializer):
     user = accounts.ReviewUserSerializer(read_only=True)
-    likes = serializers.SerializerMethodField()
+    likes_count = serializers.SerializerMethodField()
 
     class Meta:
         model = ReviewCondom
         fields = condom_fields
 
-    def get_likes(self, obj):
+    def get_likes_count(self, obj):
         content_type = ContentType.objects.get(model='review')
-        likes = Like.objects.filter(content_type=content_type.id, object_id=obj.id).count()
-        return likes
+        likes_count = Like.objects.filter(content_type=content_type.id, object_id=obj.id).count()
+        return likes_count
 
 
 class ReviewCondomSerializer(serializers.ModelSerializer):
