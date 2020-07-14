3a32d1859044ea2abf06d7cba50c95879e50070b
Jisoo Choi <cjisoo0117@gmail.com>
2020-07-14 23:08:52 +0900

    리뷰 리스트에서 좋아요 개수 표시

--- reviews/serializers/condom.py
+++ reviews/serializers/condom.py
@@ -1,8 +1,12 @@
+from django.contrib.contenttypes.models import ContentType
+
 from rest_framework import serializers
 
 from accounts.serializers import accounts
+from likes.serializers.like import LikeSerializer
 
 from reviews.models import ReviewCondom
+from likes.models import Like
 
 
 condom_filelds = [
@@ -18,15 +22,23 @@ condom_filelds = [
     "thickness",
     "durability",
     "content",
+    "likes",
 ]
 
 
 class ReviewCondomListSerializer(serializers.ModelSerializer):
     user = accounts.ReviewUserSerializer(read_only=True)
+    likes = serializers.SerializerMethodField()
 
     class Meta:
         model = ReviewCondom
         fields = condom_filelds
+
+    def get_likes(self, obj):
+        content_type = ContentType.objects.get(model='review')
+        print(content_type)
+        likes = Like.objects.filter(content_type=content_type.id, object_id=obj.id).count()
+        return likes
 
 
 class ReviewCondomSerializer(serializers.ModelSerializer):
