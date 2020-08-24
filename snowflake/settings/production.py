c36019d01944c97f0e64d75d36aba0ea7602d2ce
Jichang Jang <devjang@crossangle.io>
2020-08-24 22:55:46 +0900

    Add charset utf8mb4

--- snowflake/settings/production.py
+++ snowflake/settings/production.py
@@ -16,7 +16,8 @@ DATABASES = {
         'HOST': env('MYSQL_HOST'),
         'PORT': env('MYSQL_PORT'),
         'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset' : 'utf8mb4'
         }
     }
 }
