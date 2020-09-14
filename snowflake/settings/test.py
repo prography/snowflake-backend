8b0dc60b6b54c0063f01c892c6c8c395aeda16ea
minsung <rkdalstjd9@naver.com>
2020-09-14 23:56:43 +0900

    database 세팅

--- snowflake/settings/test.py
+++ snowflake/settings/test.py
@@ -2,28 +2,34 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 from .base import *
+import pymysql
 
 DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!
 ALLOWED_HOSTS = ['*']
 
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('PRODUCTION_MYSQL_NAME'),
+        'NAME': env('TEST_MYSQL_NAME'),
         'USER': env('MYSQL_USER'),
         'PASSWORD': env('MYSQL_PASSWORD'),
-        'HOST': env('PRODUCTION_MYSQL_HOST'),
+        'HOST': env('TEST_MYSQL_HOST'),
         'PORT': env('MYSQL_PORT'),
         'OPTIONS': {
             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
-            'charset' : 'utf8mb4'
+            'charset' : 'utf8mb4',
         }
     }
 }
 
-AWS_STORAGE_BUCKET_NAME=env("PRODUCTION_AWS_STORAGE_BUCKET_NAME")
+
+AWS_STORAGE_BUCKET_NAME=env("TEST_AWS_STORAGE_BUCKET_NAME")
 AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
 
 def sentry_before_send(event, hint):
     ignored_exceptions = [KeyboardInterrupt]
@@ -41,5 +47,5 @@ sentry_sdk.init(
     send_default_pii=True,
     attach_stacktrace=True,
     before_send=sentry_before_send,
-    environment='production'
+    environment='test'
 )
