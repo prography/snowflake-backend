5a539e93ff6ce9f0afe5c04baa11e2e8f0914663
Jichang Jang <devjang@crossangle.io>
2020-09-29 00:22:15 +0900

    예외 추가

--- snowflake/exception.py
+++ snowflake/exception.py
@@ -3,3 +3,11 @@ from rest_framework.exceptions import APIException
 class MissingProductIdException(APIException):
     status_code = 400
     default_detail = 'product id는 필수입니다.'
+
+class JWTDecodeError(APIException):
+    status_code = 400
+    default_detail = '올바르지 않은 토큰입니다. 다시 시도해주세요.'
+
+class InvalidEmailError(APIException):
+    status_code = 400
+    default_detail = '이메일 정보를 읽어오는데 실패했습니다. 다시 시도해주세요.'
