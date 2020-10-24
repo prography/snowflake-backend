e2ba3bfd2f4b73eda64e0c83c4c94211c2258c63
minsung <rkdalstjd9@naver.com>
2020-10-24 16:52:20 +0900

    url name 변경

--- labs/urls.py
+++ labs/urls.py
@@ -6,7 +6,13 @@ from .views import SutraListView, EvaluationView
 app_name = 'labs'
 
 urlpatterns = [
+    # 추후삭제 예정
     path('sutra/', SutraListView.as_view(), name='sutra'),
     path('sutra/evaluation/<int:sutra_id>/',
+         EvaluationView.as_view(), name='sutra'),
+
+    # url name의 문제로 수정
+    path('sutras/', SutraListView.as_view(), name='sutra'),
+    path('sutras/<int:sutra_id>/evaluation/',
          EvaluationView.as_view(), name='sutra'),
 ]
