a41700b1ebde92c5be66956d6b04ee16f33d129c
Jisoo Choi <cjisoo0117@gmail.com>
2020-11-22 14:22:47 +0900

    Sutra Comment > 좋아요 시그널 처리, 사용자에 따라 좋아요 true/false 보이게 함

--- labs/serializers/comment.py
+++ labs/serializers/comment.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
 from labs.models import SutraComment
+from likes.models import Like
 
 comment_fields = [
     "id",
@@ -28,6 +30,22 @@ class SutraCommentUserSerializer(serializers.ModelSerializer):
 
 class SutraCommentListSerializer(serializers.ModelSerializer):
     user = SutraCommentUserSerializer(read_only=True)
+    is_user_like = serializers.SerializerMethodField()
+
+    def get_is_user_like(self, obj) -> bool:
+        user = self.context['request'].user
+        if user.is_anonymous:
+            return False
+
+        try:
+            Like.objects.get(
+                user=user,
+                object_id=obj.id,
+                content_type=ContentType.objects.get(model='sutracomment').id
+            )
+        except Like.DoesNotExist:
+            return False
+        return True
 
     class Meta:
         model = SutraComment
@@ -36,6 +54,7 @@ class SutraCommentListSerializer(serializers.ModelSerializer):
             "user",
             "user_position",
             "content",
+            "is_user_like",
             "likes_count",
             "created_at",
             "updated_at"
